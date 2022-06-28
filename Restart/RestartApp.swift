23c2b93a04bf18ad3a5ad578298ae525643c7a00
Manuel Martinez <anymus89hd@gmail.com>
2022-06-27 22:25:42 -0500

    Initial Commit

---
+++ Restart/RestartApp.swift
@@ -0,0 +1,17 @@
+//
+//  RestartApp.swift
+//  Restart
+//
+//  Created by Manuel Martinez on 27/06/22.
+//
+
+import SwiftUI
+
+@main
+struct RestartApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
